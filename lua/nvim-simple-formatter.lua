43773cb099b90e317a65a5d4eaac2c6f087a42fd
krile136 <krile136cm@gmail.com>
2024-01-11 22:10:10 +0900

    fix

--- lua/nvim-simple-formatter.lua
+++ lua/nvim-simple-formatter.lua
@@ -1,17 +1,17 @@
 local M = {}
 
-M.getFullBufferText = function(bufnr)
-  local text = table.concat(vim.api.nvim_buf_get_lines(bufnr, 0 -1, true), "\n")
+M.getBufferText = function(bufnr)
+  local text = table.concat(vim.api.nvim_buf_get_lines(bufnr, 0, -1, true), "\n")
   if vim.api.nvim_buf_get_option(bufnr, "eol") then
     text = text .. "\n"
   end
   return text
 end
 
-M.formatBuffer = function(cmd)
+M.formatFile = function(cmd)
   local bufnr = vim.fn.bufnr("%")
-  local input = M.getFullBufferText(bufnr)
-  local output = vim.fn.system(cmd, buffer_text)
+  local input = M.getBufferText(bufnr)
+  local output = vim.fn.system(cmd, input)
   if output ~= input then
     local new_lines = vim.fn.split(output, "\n")
     vim.api.nvim_buf_set_lines(0, 0, -1, false, new_lines)
@@ -20,16 +20,20 @@ end
 
 M.createFormatFunction = function(cmd)
   return function()
-    M.formatBuffer(cmd)
+    M.formatFile(cmd)
   end
 end
+
+M.options = {}
 
 M.setup = function(options)
   M.options = vim.tbl_deep_extend("force", {}, M.options, options or {})
   for _, rule in ipairs(options.format_rules) do
-    vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-      pattern = rule.pattern,
-      callback = M.createFormatFunction(rule.command)
-    })
+      vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+        pattern = rule.pattern,
+        callback = M.createFormatFunction(rule.command),
+      })
   end
 end
+
+return M
